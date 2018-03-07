f5880f5ae8ab94d872c7a46ce059b3d3110d0d15
Francisco Paz-Chinchón <fpazch@illinois.edu>
2018-03-07 17:05:09 -0600

    Improving to be more flexible, Y5A1

--- createBPM.py
+++ createBPM.py
@@ -1,112 +1,182 @@
-'''Script to create BPM based in preBPM, using the directions from Robert.
+'''Script to create BPM based in preBPM and Precal products, using the 
+BPM code creation wrote by RGruendl
 '''
 
 import os
 import sys
+import socket
+import uuid
 import subprocess
 import time
+import logging
 import numpy as np
 import shlex
+# setup logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                   )
 
-class Toolbox():
-    @classmethod
-    def progress_bar(cls,iterator,Nposit,wait_time=0.25):
-        '''Receives the actual iterator and the max number of items 
+
+class Listed():
+    def __init__(self, 
+                 fn_pixcor=None, 
+                 fn_precal=None, 
+                 ccd=None,
+                 badpix=None,
+                 bindir=None,
+                 band=None,
+                 root_dir='/archive_data/desarchive/'):
+        '''
+        Load the two inputs tables, assuming no compression in the 
+        pixcor (fn_pixcor) files. Also define variables for the execution
+        of mkbpm
+        '''
+        # Pending improvemet: accept user-defined dtypes and tables with
+        # and use only selected columns
+        aux_dt1 = [('expnum', 'i4'),
+                   ('ccdnum', 'i4'),
+                   ('band', '|S10'),
+                   ('root', '|S100'),
+                   ('path', '|S100'),
+                   ('filename', '|S50'),
+                   ('compression', '|S50'),
+                  ]
+        # if compression:
+        #     aux_dt1.append(('compression','|S10'))
+        dt1 = np.dtype(aux_dt1)
+        dt2 = np.dtype([('archive_path', '|S100'),
+                        ('reqnum', 'i4'),
+                        ('unitname', 'i4'),
+                        ('attnum', 'i4')])
+        A = np.loadtxt(fn_pixcor, 
+                       dtype=dt1, 
+                       comments='#',
+                       skiprows=0,
+                       usecols=None)
+        B = np.loadtxt(fn_precal,
+                       dtype=dt2,
+                       comments='#',
+                       skiprows=0,
+                       usecols=None)
+        self.obj = A
+        self.precal = B
+        logging.info('{0} and {1} loaded as recarray'.format(badpix, bindir))
+        # For the variables
+        self.ccd = ccd
+        self.badpix = badpix
+        self.bindir = bindir
+        self.band = band
+        self.root_dir = root_dir
+    
+    def progress_bar(self, iterator, Nposit, wait_time=0.25):
+        '''
+        Function receives the actual iterator and the max number of itemss
+        then displays a progress bar.
         Idea from: http://stackoverflow.com/questions/3002085/
         python-to-print-out-status-bar-and-percentage
         '''
         sys.stdout.write('\r')
-        aux = (iterator*100/Nposit)-1
-        sys.stdout.write('|{0:{1}}| {2}%'.format('='*iterator,Nposit,aux))
+        aux = (iterator * 100 / Nposit) - 1
+        sys.stdout.write('|{0:{1}}| {2}%'.format('=' * iterator,
+                                                 Nposit, aux))
         sys.stdout.flush()
         time.sleep(wait_time)
+        return True
 
-
-class Listed():
-    def __init__(self,fn_pixcor,fn_precal,compression=False):
-        '''Load the two inputs tables, assuming no compression in the 
-        pixcor (fn_pixcor) files
-        '''
-        #IMPROVEMENT: accept user-defined dtypes and tables with
-        #other elements, to use only selected columns
-        aux_dt1 = [('expnum','i4'),('ccdnum','i4'),('band','|S10'),
-                ('root','|S100'),('path','|S100'),('filename','|S50')]
-        if compression:
-            aux_dt1.append(('compression','|S10'))
-        dt1 = np.dtype(aux_dt1)
-        dt2 = np.dtype([('archive_path','|S100'),('reqnum','i4'),
-                    ('unitname','i4'),('attnum','i4')])
-        A = np.loadtxt(fn_pixcor,dtype=dt1,comments='#',skiprows=0,usecols=None)
-        B = np.loadtxt(fn_precal,dtype=dt2,comments='#',skiprows=0,usecols=None)
-        self.pieces = A
-        self.precal = B
-
-    @classmethod
-    def feed_list(cls,fn_pixcor,fn_precal,compression,ccdnum,BPM_band):
+    def feed_list(self):
         '''Create lists to be used as inputs for the BPM creation code
         '''
-        prec = Listed(fn_pixcor,fn_precal,compression).precal
+        aux_m = 'Creating lists of biascor, norm-dflatcor, and pixcor.'
+        aux_m += ' Note the first two can be easily achieved with DB queries'
+        logging.info(aux_m)
         biascor = []
         flatcor = []
-        for n in xrange(prec.shape[0]):
-            for iter_ccd in ccdnum:
-                fn_bias = 'D_n{0}_c{1:02}_r{2}p{3:02}_biascor.fits'.format(
-                    prec['unitname'][n],iter_ccd,prec['reqnum'][n],
-                    prec['attnum'][n])
-                aux_line = os.path.join('/archive_data/desarchive',
-                                    *[prec['archive_path'][n],
-                                    'biascor',fn_bias])
-                biascor.append((aux_line,iter_ccd))
-                for bb in BPM_band:
-                    fn_flat = 'D_n{0:08}_{1}_c{2:02}_r{3}p{4:02}'.format(
-                        prec['unitname'][n],bb,iter_ccd,
-                        prec['reqnum'][n],prec['attnum'][n])
-                    fn_flat += '_norm-dflatcor.fits'
-                    tmp_line = os.path.join('/archive_data/desarchive',
-                                        *[prec['archive_path'][n],
-                                        'norm-dflatcor',fn_flat])
-                    flatcor.append((tmp_line,iter_ccd))
-        #CHECK THIS: its ok to only use g-band on flatcor list?
-        ccdN = Listed(fn_pixcor,fn_precal,compression).pieces
-        construct = lambda x,y,z,ccd: (os.path.join(x,*[y,z]),ccd)
-        obj = [construct(x['root'],x['path'],x['filename'],x['ccdnum']) 
-            for x in ccdN]
-        dt = np.dtype([('path','|S200'),('ccdnum','i4')])
+        for n in xrange(self.precal.shape[0]):
+            for iter_ccd in self.ccd:
+                # Construct biascor list from precal list. One per CCD
+                # Here unitname is nite
+                reqnum = self.precal['reqnum'][n]
+                unitname = self.precal['unitname'][n]
+                attnum = self.precal['attnum'][n]
+                fn_bias = 'D_n{0}_c{1:02}_r{2}p{3:02}'.format(unitname,
+                                                              iter_ccd,
+                                                              reqnum,
+                                                              attnum)
+                fn_bias += '_biascor.fits'
+                aux_line = os.path.join(self.root_dir,
+                                        self.precal['archive_path'][n],
+                                        'biascor',
+                                        fn_bias)
+                biascor.append((aux_line, iter_ccd))
+                # Create list of norm-dflatcor 
+                fn_flat = 'D_n{0}_{1}_c{2:02}_r{3}p{4:02}'.format(unitname,
+                                                                  self.band,
+                                                                  iter_ccd,
+                                                                  reqnum,
+                                                                  attnum)
+                fn_flat += '_norm-dflatcor.fits'
+                tmp_line = os.path.join(self.root_dir,
+                                        self.precal['archive_path'][n],
+                                        'norm-dflatcor',
+                                        fn_flat)
+                flatcor.append((tmp_line,   iter_ccd))
+        # Using the objects list (from preBPM), merge the path and filename.
+        # Also save the ccdnum for eachi full path
+        f_merge = lambda x, y, z, ccd: (os.path.join(x, y, z), ccd)
+        obj = [f_merge(x['root'], x['path'], x['filename'], x['ccdnum']) 
+               for x in self.obj]
+        dt = np.dtype([('path', '|S200'),
+                       ('ccdnum', 'i4')])
+        obj = np.array(obj,dtype=dt) 
         biascor = np.array(biascor,dtype=dt)
         flatcor = np.array(flatcor,dtype=dt)
-        obj = np.array(obj,dtype=dt) 
-        return biascor,flatcor,obj
+        return biascor, flatcor, obj
     
-    @classmethod
-    def make_bpm(cls,fn_pixcor,fn_precal,compression=False,
-                ccdnum=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-                21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
-                41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,
-                62],
-                BPM_band=['g'],
-                badpix_dir='/work/devel/fpazch/desdmSVN_copy/devel/despycal/trunk/data',
-                exe_dir='/work/devel/fpazch/desdmSVN_copy/devel/despycal/trunk/bin',
-                label='test',
-                clean_tmp=True):
+    def make_bpm(self, clean_tmp=True):
         '''Method to run the code and fill the logs, using the PID as 
         auxiliary for filename/path creation
         '''
         PID = os.getpid()
-        biascor,flatcor,obj = Listed.feed_list(fn_pixcor,fn_precal,
-                                            compression,ccdnum,BPM_band)
-        #create output folder
-        dir_out = 'out_{0}_pid{1}'.format(label,PID)
-        dir_log = 'log_{0}_pid{1}'.format(label,PID)
-        mkout = subprocess.check_call(['mkdir',dir_out])
-        mklog = subprocess.check_call(['mkdir',dir_log])
-
-        #BPM call, per CCD
-        print '\tBPM per CCD\n\t%s'%('^'*11)
-        for m in ccdnum:
-            Toolbox.progress_bar(m,len(ccdnum),wait_time=0.5)
-            #write out tmp files
+        bias_aux, flat_aux, obj_aux = self.feed_list()
+        # Create output folders
+        dir_out = 'out_{0}_pid{1}'.format(label, PID)
+        dir_log = 'log_{0}_pid{1}'.format(label, PID)
+        #
+        if os.path.exists(os.path.dirname(dir_out)):
+            aux_m1 = 'Directory {0} exists. Creating new name'.format(dir_out)
+            logging.warning(aux_m1)
+            dir_out = 'out_{0}_{1}'.format(label, str(uuid.uuid4())) 
+            aux_m2 = 'Created: {0}'.format(dir_out)
+        else:
+            try:
+                os.makedirs(os.path.dirname(dir_out))
+            except:
+                e = sys.exc_info()[0]
+                logging.error(e)
+                exit(1)
+        #
+        if os.path.exists(os.path.dirname(dir_log)):
+            aux_m1 = 'Directory {0} exists. Creating new name'.format(dir_log)
+            logging.warning(aux_m1)
+            dir_log = 'out_{0}_{1}'.format(label, str(uuid.uuid4())) 
+            aux_m2 = 'Created: {0}'.format(dir_log)
+        else:
+            try:
+                os.makedirs(os.path.dirname(dir_log))
+            except:
+                e = sys.exc_info()[0]
+                logging.error(e)
+                exit(1)
+        # BPM call, per CCD
+        logging.info('Call of BPM creation')
+        for m in self.ccd:
+            self.progress_bar(m, len(self.ccd), wait_time=0.5)
+            # Write out tmp files, using auxiliary arrays created in 
+            # feed_list() method
             out_bias = 'pid{0}.biascor.csv'.format(PID)
-            np.savetxt(out_bias,biascor[biascor['ccdnum']==m]['path'],fmt='%s')
+            np.savetxt(out_bias, 
+                       biascor[biascor['ccdnum'] == m]['path'],
+                       fmt='%s')
             out_flat = 'pid{0}.flatcor.csv'.format(PID)
             np.savetxt(out_flat,flatcor[flatcor['ccdnum']==m]['path'],fmt='%s')
             out_obj = 'pid{0}.objects.csv'.format(PID)
@@ -153,9 +223,57 @@ class Listed():
 
 
 if __name__=='__main__':
-    '''use_ccd = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-            21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
-            41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,
-            62]
-    '''
-    Listed.make_bpm('y4e2_object.list','y4e2_precal.list',label='y4e2')
+    logging.info('Running on {0}'.format(socket.gethostname()))
+    #
+    ccd_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+                21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
+                41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,
+                62]
+    badpix_dir = '/work/devel/fpazch/desdmSVN_copy/devel/despycal/trunk/data'
+    exec_dir='/work/devel/fpazch/desdmSVN_copy/devel/despycal/trunk/bin'
+    band = 'g'
+    #
+    desc = 'Script to collect lists and iteratively call RGruendl \'mkbpm\''
+    desc += ' for BPM creation, CCD by CCD. This uses PRECAL and PREBPM'
+    desc += ' products as ingredients. Note BPMs are constructed based on'
+    desc += ' g-band'
+    abc = argparse.ArgumentParser(description=desc)
+    #
+    t0 = 'Object table harboring \'red_pixcor\' information, from PREBPM.'
+    t0 += ' Columns must be (in order): EXPNUM, CCDNUM, BAND, ROOT, PATH'
+    t0 += ' FILENAME, COMPRESSION'
+    abc.add_argument('objects', help=t0, type=str)
+    t1 = 'Precal table harboring PRECAL products information. Columns must'
+    t1 += ' be (in order): ARCHIVE_PATH, REQNUM, UNITNAME, ATTNUM'
+    abc.add_argument('precal', help=t1, type=str)
+    t2 = 'Label to be used for generated BPM'
+    abc.add_argument('--label', help=t2, metavar='')
+    t3 = 'Space separated list of CCD numbers to be used. Default:'
+    t3 += ' {0}'.format(ccd_list)
+    abc.add_argument('--ccd', help=t3, nargs=+)
+    t4 = 'Directory for badpixel lists definitions. Default: '
+    t4 += ' {0}'.format(badpix)
+    abc.add_argument('--badpix', help=t4, metavar='', default=badpix_dir)
+    t5 = 'Directory for executables. Default: {0}'.format(exec_dir)
+    abc.add_argument('--bindir', help=t5, metavar='', default=exec_dir)
+    t6 = 'Band. Default: {0}'.format(band)
+    abc.add_argument('--band', help=t6, metavar='', default=band)
+    # Parse
+    abc = abc.parse_args()
+    #
+    if (abc.label is None):
+        label = str(uuid.uuid4())
+    else:
+        label = abc.label
+    if (str.lower(abc.band) != 'b'):
+        logging.warning('Band is not g-band')
+    #
+    L = Listed(
+            fn_pixcor=abc.objects,
+            fn_precal=abc.precal,
+            ccd=abc.ccd,
+            badpix=abc.badpix,
+            bindir=abc.bindir,
+            band=abc.band
+        )
+    L.make_bpm()
