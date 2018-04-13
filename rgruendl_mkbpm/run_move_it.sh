8a1baa5a73b26bd47607ae98fae1e305a16b5a85
Francisco Paz-Chinchón <fpazch@illinois.edu>
2018-04-13 14:59:23 -0500

    redundant erased

--- rgruendl_mkbpm/run_move_it.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo Running MOVE_IT.CSH, script of Robert to create the final filenames to be ingested
-csh -f move_it.csh out_y4e1 20160921t1003-r2901/p01 'D_n20160921t1003' 'r2901p01_bpm' 'yes' 
